971b39cefb7b21fbbf5806a1d112505c2123983a
guillermo.arispe <guillermo.arispe@gmail.com>
2026-07-25 12:19:09 -0300

    fix(example): actualizar limites del SDK, corregir headline4 y solucionar inicializacion de widget test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -7,11 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mtc_analytics/mtc_analytics.dart';
 
 import 'package:example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    AnalyticsService.instance.init([ConsoleTracker()]);
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
